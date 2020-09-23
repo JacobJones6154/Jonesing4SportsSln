aee1223100e6df4d8abcb3b0e202324227d96cb6
JacobJones6154 <68295733+JacobJones6154@users.noreply.github.com>
2020-09-23 10:04:27 -0700

    Updated Chapter 9

--- OutdoorProducts/Controllers/OrderController.cs
+++ OutdoorProducts/Controllers/OrderController.cs
@@ -4,34 +4,35 @@ using System.Linq;
 
 namespace OutdoorProducts.Controllers
 {
+
     public class OrderController : Controller
+{
+    private IOrderRepository repository;
+    private Cart cart;
+    public OrderController(IOrderRepository repoService, Cart cartService)
     {
-        private IOrderRepository repository;
-        private Cart cart;
-        public OrderController(IOrderRepository repoService, Cart cartService)
+        repository = repoService;
+        cart = cartService;
+    }
+    public ViewResult Checkout() => View(new Order());
+    [HttpPost]
+    public IActionResult Checkout(Order order)
+    {
+        if (cart.Lines.Count() == 0)
         {
-            repository = repoService;
-            cart = cartService;
+            ModelState.AddModelError("", "Sorry, your cart is empty!");
         }
-        public ViewResult Checkout() => View(new Order());
-        [HttpPost]
-        public IActionResult Checkout(Order order)
+        if (ModelState.IsValid)
         {
-            if (cart.Lines.Count() == 0)
-            {
-                ModelState.AddModelError("", "Sorry, your cart is empty!");
-            }
-            if (ModelState.IsValid)
-            {
-                order.Lines = cart.Lines.ToArray();
-                repository.SaveOrder(order);
-                cart.Clear();
-                return RedirectToPage("/Completed", new { orderId = order.OrderID });
-            }
-            else
-            {
-                return View();
-            }
+            order.Lines = cart.Lines.ToArray();
+            repository.SaveOrder(order);
+            cart.Clear();
+            return RedirectToPage("/Completed", new { orderId = order.OrderID });
+        }
+        else
+        {
+            return View();
         }
     }
+}
 }
